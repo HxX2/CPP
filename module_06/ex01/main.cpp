034b7daf22e7612d7c7a2ffa145673f36809aa74
HxX2 <zaka.hanix@gmail.com>
2023-10-16 12:22:25 +0100

    add tests

--- module_06/ex01/main.cpp
+++ module_06/ex01/main.cpp
@@ -6,12 +6,17 @@ int main()
 	Data *data = new Data;
 	data->name = "John";
 	data->age = 42;
+	std::cout << "Data address: " << data << std::endl;
+	std::cout << "------" << std::endl;
 	uintptr_t raw = serializer.serialize(data);
 	std::cout << "Serialized Data: " << raw << std::endl;
 	Data *data2 = serializer.deserialize(raw);
+	std::cout << "------" << std::endl;
 	std::cout << "Deserialized Data: " << std::endl;
 	std::cout << "name: " << data2->name << std::endl;
 	std::cout << "age: " << data2->age << std::endl;
+	std::cout << "------" << std::endl;
+	std::cout << "Data address: " << data << std::endl;
 	delete data;
 	return (0);
 }

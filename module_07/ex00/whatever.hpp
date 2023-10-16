008e749691d824429307a5d263d7c5483febb231
HxX2 <zaka.hanix@gmail.com>
2023-10-16 16:37:02 +0100

    ex00

---
+++ module_07/ex00/whatever.hpp
@@ -0,0 +1,26 @@
+#ifndef _WHATEVER_HPP_
+#define _WHATEVER_HPP_
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T min(T &a, T &b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T>
+T max(T &a, T &b)
+{
+	return (a > b ? a : b);
+}
+
+#endif
